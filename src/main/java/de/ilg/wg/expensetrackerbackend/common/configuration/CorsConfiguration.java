7d3776e6a660405c79ff6cabb4b0dc83d1fa004b
IlgT <tobias.ilg@gmx.net>
2020-05-18 23:50:52 +0200

    Enable CORS for angular front end

--- src/main/java/de/ilg/wg/expensetrackerbackend/common/configuration/CorsConfiguration.java
+++ src/main/java/de/ilg/wg/expensetrackerbackend/common/configuration/CorsConfiguration.java
@@ -13,7 +13,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/FASA/api/**")
+        registry.addMapping("/**")
         .allowedOrigins("http://localhost:4200")
         .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
